a065841a69e4881ac75c4e0f66691d37aa6699b6
Afzal640 <afzal@example.com>
2026-05-08 11:59:25 +0500

    Updated API URL to production

--- frontened/src/app/api/api.js
+++ frontened/src/app/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // Example: src/api/axios.js ya jahan bhi baseURL hai
-baseURL: "https://mt-managwemnet.vercel.app/api"
+  baseURL: import.meta.env.VITE_API_URL, // Ye line check karein
+  withCredentials: true
 });
 
 api.interceptors.request.use((config) => {
